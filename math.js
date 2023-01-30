9b11d9154316d802c5629afa971ec1f054e02a2d
farhanthegreat <farhantuber@gmail.com>
2023-01-31 00:47:19 +0600

    third added

--- math.js
+++ math.js
@@ -3,6 +3,13 @@
  * 2. integer, float, double
  * 3. operation, addition, substraction, multiplication, division, reminder, modulus, percent, average, comparison, greater than >, smaller than <, ==, !=, <=, ?=
  * 4. oikik niyom, 1 ta eto hole 10 ta koto?
- * 5. 
+ * 5. 45, 55, 34, 66, max, min, avg, sum, 
+ * 6. series: sum, odd, even, 
+ * extra: 
+ * prime number, permutation, area, trigonometry,  
+ * 
+ * always mind set:::::
+ * ja lagbe ta shikhar mindset lagbei.
+ *  
  * 
  */
