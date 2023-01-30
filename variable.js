0f4dcd04f0dd75265ce0c432d8b72d76718b2975
farhanthegreat <farhantuber@gmail.com>
2023-01-31 01:39:30 +0600

    string boolean data type added

--- variable.js
+++ variable.js
@@ -1,5 +1,17 @@
+// numeric type
 var perfume = 150;
 var charge = 120;
 var packaging = 20;
 var weight = 60;
 var money = 110;
+
+// string type
+var product_name = "cool water";
+var product_type = "perfume oil";
+var speciality = 'long lasting';
+
+// boolean type
+
+var serious = true;
+var full_marks = false;
+
