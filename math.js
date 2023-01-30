a6999fa2eaaab8e4e39bea333dbec68aa488d700
farhanthegreat <farhantuber@gmail.com>
2023-01-31 00:42:36 +0600

    second added

---
+++ math.js
@@ -0,0 +1,8 @@
+/**
+ * 1. number
+ * 2. integer, float, double
+ * 3. operation, addition, substraction, multiplication, division, reminder, modulus, percent, average, comparison, greater than >, smaller than <, ==, !=, <=, ?=
+ * 4. oikik niyom, 1 ta eto hole 10 ta koto?
+ * 5. 
+ * 
+ */
