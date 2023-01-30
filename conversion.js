93fbe388dc8b4b9910bbc33231885b4bac743fe2
farhanthegreat <farhantuber@gmail.com>
2023-01-31 03:10:40 +0600

    introduction to javascript is done

---
+++ conversion.js
@@ -0,0 +1,16 @@
+var price1 = '30';
+var price2 = 10;
+var totalPrice = price1 + price2;
+console.log(totalPrice);
+
+//convert string to number
+
+console.log(price1);
+var price1 = parseInt(price1);
+console.log(price1);
+console.log(price2);
+console.log(totalPrice);
+
+var gpa = '3.41';
+var gpaNumber = parseFloat(gpa);
+console.log(gpaNumber);
---
+++ remainder.js
@@ -0,0 +1,6 @@
+var mangos = 7;
+var eater = 2;
+console.log(mangos % eater);
+
+// / this means division
+// % this means modulus 
