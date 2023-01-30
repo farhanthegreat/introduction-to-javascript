7dd72ff8c3de29f7435c744505846bcae5056b2a
farhanthegreat <farhantuber@gmail.com>
2023-01-31 02:34:51 +0600

    variable operation added

---
+++ shorthand.js
@@ -0,0 +1,34 @@
+var tomatoPrice = 25;
+tomatoPrice = 30; //variable value updated
+
+// add something 
+
+var price1 = 31;
+var price2 = 10;
+
+//add some value to existing value
+price1 = price1 + 10;
+// console.log(price1);
+
+//addition to the same variable shorthand
+//+= means adding some value to the existing value.
+
+price1 +=10;
+
+console.log(price1);
+
+price2 -=5;
+console.log(price2);
+
+price2 *=5;
+console.log(price2);
+
+price2 /= 5;
+console.log(price2);
+
+var age = 15;
+//++ means adding 1 to the existing value.
+age++;
+
+var love = 100;
+love--;
