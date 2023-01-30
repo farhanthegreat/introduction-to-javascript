bd32745954ae3a79658764c8b224c4b308c43035
farhanthegreat <farhantuber@gmail.com>
2023-01-31 01:56:10 +0600

    variable added

---
+++ variable-name.js
@@ -0,0 +1,21 @@
+// perfect variable
+var price = 30;
+
+
+// 1. variable name can not be a reserved or keyword name.
+
+// 2. variable name has to be in one word. no space between
+
+// 3. variable name can not have quotation.
+// 4. variable name can not contain first letter in number. but can ends with number.
+
+// 5. how to use long name: 
+// can not use dash.
+var user_name = "tanvir";
+var user_home_address = "my house"; //snake case
+
+var userHomeAddress = "tanvir";// camel case we will use this case.
+var UserHomeAddress = "tanvir"; //pascal case
+
+
+
